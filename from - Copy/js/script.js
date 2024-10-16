ce488f5e096a2d9c492a1108c41b4fc6624bbf27
MD. Montasir Alam <166697994+montasir132@users.noreply.github.com>
2024-10-17 00:53:57 +0600

    Add files via upload

---
+++ from - Copy/js/script.js
@@ -0,0 +1,16 @@
+/*================= js ==================*/ 
+const signupBtn = document.getElementById("signup-btn");
+const mainContainer = document.querySelector(".container");
+
+signupBtn.addEventListener("click", () => {
+  mainContainer.classList.toggle("change");
+});
+signinBtn.addEventListener("click", () => {
+  mainContainer.classList.toggle("change");
+});
+
+
+/*================= jq ==================*/
+$(document).ready(function(){
+
+});
---
+++ from/js/script.js
@@ -0,0 +1,17 @@
+/*================= js ==================*/ 
+const signupBtn = document.getElementById("signup-btn");
+const signinBtn = document.getElementById("signin-btn");
+const mainContainer = document.querySelector(".container");
+
+signupBtn.addEventListener("click", () => {
+  mainContainer.classList.toggle("change");
+});
+signinBtn.addEventListener("click", () => {
+  mainContainer.classList.toggle("change");
+});
+
+
+/*================= jq ==================*/
+$(document).ready(function(){
+
+});
